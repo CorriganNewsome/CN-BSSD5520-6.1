392993f636f25881670328798926ae67131a8f90
Corrigan Newsome <corrigannewsome@gmail.com>
2021-02-26 13:02:01 -0700

    added .zip file contents

---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+Complete these tasks which shoudl be review from previous problems.
+
+****************************************************
+****** Submit via github before 10AM Monday. *******
+****************************************************
+
+Remember to do your own work.
+
+You may add elements to the html unless otherwise told to use only javascript in a specific task.
+
+Overview: The user will be able to type their name and choose from a list one of the three job titles. When they slick submit, they will see their name and the job title in a div and an image based on the job title they chose.
+
+Task 1: Create an input element in html and store the user's typed name in a javascript variable when they press the submit button.
+
+Task 2: Create an empty select element in the html. Add any 3 job types as options of the selector via javascript (i.e Fireman, Mage, Doctor, Robot).
+
+Task 3: When the user presses submit, if there is no name in the input field, an error message should say "Please enter your name first"
+
+Task 4: When the user presses submit, if there is a name in the input field, the input field, submit button, and select element should go away and a div should say the job the person chose followed by their name (i.e. Doctor Fred). There is no way to enter the information again.
+
+Task 5: When the user presses submit, in addition to what occured in Task 4, an image should also display via an img tag underneath their name. The image should be based on the job chosen (i.e. Robot Bob should show a picture of a robot under the text "Robot Bob")
+
+Each task is worth 20 points for a total of 100 points on the assignment.
+*/
+
+/* Arrow Function
+
+let name = (parameter(s)) => {
+  //code to be executed
+}
+
+*/
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {});
